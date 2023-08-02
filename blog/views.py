e2a8a6498e250ed8e874572b01b6008746260d22
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-08-02 11:28:32 -0300

    atualizacao 02.08

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,5 @@ def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
 def ola(request):
-    return HttpResponse('Olá Django')
+    #return HttpResponse('Olá Django')
+    return render(request, 'home.html')
