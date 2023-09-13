18df3e53779649426c87594d02fd92255f679a3b
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-09-13 11:06:04 -0300

    CREATE - CRUD Básico.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from blog.views import (
     index, ola,  post_show, PostDetailView, 
-    get_all_posts, get_post )
+    get_all_posts, get_post, PostCreateView, create_post )
 
 urlpatterns = [
     path('index/', index, name='index'),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
     path('api/posts', get_all_posts, name="posts_data"),
     path('api/posts/<int:post_id>', get_post, name="post_data"),
+    path('post/add', PostCreateView.as_view(), name="post_add"),
+    path('api/post/add', create_post, name="create_post_data"),
 ]
