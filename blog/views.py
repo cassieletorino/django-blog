3bb55c3c88fdcd56662b452f4d5af00cc6350aaa
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-09-27 10:45:33 -0300

    Introdução aos forms. (27/09)

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import DetailView, ListView, TemplateView
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post
+from blog.forms import PostModelForm
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.views.generic.edit import CreateView
@@ -71,8 +72,9 @@ def get_post(request, post_id):
 class PostCreateView(CreateView):
     model = Post
     template_name = 'post/post_form.html'
-    fields = ('body_text',)
+    # fields = ('body_text',)
     success_url = reverse_lazy('posts_list')
+    form_class = PostModelForm
 
 @csrf_exempt
 def create_post(request):
