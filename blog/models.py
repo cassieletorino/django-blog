7fbefb5dd5f4693a1c69ac420173f3f98b055d4d
Cassiele Torino <ketlin.cassi121@gmail.com>
2023-11-01 10:08:48 -0300

    Post com autor

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model 
+User = get_user_model()
+
 class Post(models.Model):
     body_text = models.TextField('Texto Principal')
     pub_date = models.DateTimeField('Data Publicação')
@@ -13,4 +16,10 @@ class Post(models.Model):
         ],
         default=None,
         null=True
+    )
+    autor= models.ForeignKey(
+        User, # chave estrangeira vinculada ao usuário 
+        editable=False, # não permite editar 
+        on_delete=models.DO_NOTHING, # não exclui a pergunta se o autor for removido
+        null=True # permite autor NULL para não conflitar com registros já existentes
     )
